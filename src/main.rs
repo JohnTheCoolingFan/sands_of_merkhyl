9080931baed345c1bc318ba73b5b25dfa78b13e9
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-12-17 12:52:10 +0300

    Added view switching

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,32 @@ use bevy_prototype_lyon::prelude::*;
 
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const CLEAR_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
+const MAP_VIEW_SCALE: f32 = 1.0;
+const PLATFORM_VIEW_SCALE: f32 = 20.0;
 
 #[derive(Resource)]
 struct MiningPlatformSprite(Handle<Image>);
 
 #[derive(Resource)]
 struct TileSprite(Handle<Image>);
+
+#[derive(Resource)]
+enum CurrentView {
+    Platform,
+    Map,
+}
+
+impl CurrentView {
+    fn toggle(&mut self) {
+        *self = match self {
+            Self::Platform => Self::Map,
+            Self::Map => Self::Platform,
+        }
+    }
+}
+
+#[derive(Component)]
+struct Map;
 
 fn spawn_camera(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
@@ -48,16 +68,9 @@ fn spawn_platform(mut commands: Commands, sprite: Res<MiningPlatformSprite>) {
             ..default()
         },
         DrawMode::Fill(FillMode::color(Color::rgb(1.0, 0.0, 0.0))),
-        Transform::from_xyz(0.0, 0.0, 1.0),
+        Transform::from_xyz(0.0, 0.0, 100.0),
     ));
     */
-}
-
-fn load_assets(mut commands: Commands, assets: Res<AssetServer>) {
-    let mining_platform_sprite = assets.load("mining_platform.png");
-    commands.insert_resource(MiningPlatformSprite(mining_platform_sprite));
-    let tile_texture = assets.load("tile.png");
-    commands.insert_resource(TileSprite(tile_texture));
 }
 
 fn camera_movement(
@@ -86,25 +99,63 @@ fn camera_movement(
     camera_transform.translation += delta.extend(0.0);
 }
 
+fn switch_view(
+    input: Res<Input<KeyCode>>,
+    mut camera: Query<(&mut OrthographicProjection, &mut Transform), With<Camera2d>>,
+    mut map: Query<&mut Visibility, (With<Map>, Without<Camera2d>)>,
+    mut current_view: ResMut<CurrentView>,
+) {
+    if input.just_pressed(KeyCode::M) {
+        current_view.toggle();
+        match *current_view {
+            CurrentView::Map => {
+                map.single_mut().is_visible = true;
+                let (mut projection, mut cam_transform) = camera.single_mut();
+                projection.scale = MAP_VIEW_SCALE;
+                cam_transform.translation = Vec2::new(0.0, 0.0).extend(cam_transform.translation.z);
+            },
+            CurrentView::Platform => {
+                map.single_mut().is_visible = false;
+                let (mut projection, mut cam_transform) = camera.single_mut();
+                projection.scale = PLATFORM_VIEW_SCALE;
+                cam_transform.translation = Vec2::new(0.0, 0.0).extend(cam_transform.translation.z);
+            }
+        }
+    }
+}
+
+fn load_assets(mut commands: Commands, assets: Res<AssetServer>) {
+    let mining_platform_sprite = assets.load("mining_platform.png");
+    commands.insert_resource(MiningPlatformSprite(mining_platform_sprite));
+    let tile_texture = assets.load("tile.png");
+    commands.insert_resource(TileSprite(tile_texture));
+}
+
 fn main() {
     let height = 900.0;
     App::new()
         .insert_resource(ClearColor(CLEAR_COLOR))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: "Desert Stranding".to_string(),
-                present_mode: PresentMode::Fifo,
-                height,
-                width: height * ASPECT_RATIO,
-                resizable: false,
-                ..default()
-            },
-            ..default()
-        }).set(ImagePlugin::default_nearest()))
+        .insert_resource(CurrentView::Platform)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Desert Stranding".to_string(),
+                        present_mode: PresentMode::Fifo,
+                        height,
+                        width: height * ASPECT_RATIO,
+                        resizable: false,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_plugin(ShapePlugin) // bevy_prototype_lyon
         .add_startup_system_to_stage(StartupStage::PreStartup, load_assets)
         .add_startup_system(spawn_platform)
         .add_startup_system(spawn_camera)
         .add_system(camera_movement)
+        .add_system(switch_view)
         .run();
 }
